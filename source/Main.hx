11fd6e8d5bb27efd16258e36aa41e9d69d71ec47
Randomxddd <amongus@sus.com>
2022-02-06 10:36:38 -0300

    added internal storage support (no android/data anymore)

--- source/Main.hx
+++ source/Main.hx
@@ -8,10 +8,12 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
+#if android //only android will use those
 import sys.FileSystem;
 import lime.app.Application;
 import lime.system.System;
-
+import android.*;
+#end
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
@@ -21,26 +23,32 @@ class Main extends Sprite
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-
+        #if android//the things android uses  
+        private static var androidDir:String = null;
+        private static var storagePath:String = AndroidTools.getExternalStorageDirectory();  
+        #end
 	public static var fpsVar:FPS;
         public static var memoryCounter:MemoryCounter;
 
 	private static var dataPath:String = null;
 
-        static public function getDataPath():String 
+        static public function getDataPath():String
         {
-            #if android
-            if (dataPath != null && dataPath.length > 0) 
-            {
-                return dataPath;
-            } 
-            else 
-            {
-                 dataPath = "/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/";
-            }
-            return dataPath;
-            #end
+        	#if android
+                if (androidDir != null && androidDir.length > 0) 
+                {
+                        return androidDir;
+                } 
+                else 
+                { 
+                        androidDir = storagePath + "/" + Application.current.meta.get("packageName") + "/files/";
+                }
+                return androidDir;
+                #else
+                return "";
+	        #end
         }
+  
 
 	public static function main():Void
 	{
@@ -60,6 +68,8 @@ class Main extends Sprite
 			addEventListener(Event.ADDED_TO_STAGE, init);
 		}
 	}
+
+          
 
 	private function init(?E:Event):Void
 	{
@@ -89,34 +99,41 @@ class Main extends Sprite
 		initialState = TitleState;
 		#end
 
-                #if android
+               #if android
+                if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
+		    AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
+		}  
 
-                if (!FileSystem.exists("/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName")))
-                {
-                    Application.current.window.alert("Try creating A folder Called " + Application.current.meta.get("packageName") + " in Android/data/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
-                    System.exit(0);//Will close the game
-                }
-                else if (!FileSystem.exists("/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files"))
-                {
-                    Application.current.window.alert("Try creating A folder Called Files in Android/data/" + Application.current.meta.get("packageName") + "\n" + "Press Ok To Close The App", "Check Directory Error");
-                    System.exit(0);//Will close the game
-                }
-                else if (!FileSystem.exists(Main.getDataPath() + "assets"))
-                {
-                    Application.current.window.alert("Try copying assets/assets from apk to " + " /storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
-                    System.exit(0);//Will close the game
-                }
-                else if (!FileSystem.exists(Main.getDataPath() + "mods"))
-                {
-                    Application.current.window.alert("Try copying assets/mods from apk to " + " /storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
-                    System.exit(0);//Will close the game
+            var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions();    
+
+                if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
+                	if (AndroidTools.getSDKversion() > 23 || AndroidTools.getSDKversion() == 23) {
+                        	Application.current.window.alert("If you accepted the permisions for storage good, you can continue, if you not the game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App","Permissions");
+                                System.exit(0);//Will close the game
+		        } else {
+                        	Application.current.window.alert("game can't run without storage permissions please grant them in app settings" + "\n" + "Press Ok To Close The App","Permissions");
+                                System.exit(0);//Will close the game
+		        }
                 }
                 else
                 {
-                    if (!FileSystem.exists(Main.getDataPath() + "yourthings"))
-	            FileSystem.createDirectory(Main.getDataPath() + "yourthings");                   
+                        if (!FileSystem.exists(storagePath + "/" + Application.current.meta.get("packageName"))) {
+                                FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName"));
+                        } 
+                        if (!FileSystem.exists(storagePath + "/" + Application.current.meta.get("packageName") + '/files')) {
+                                FileSystem.createDirectory(storagePath + "/" + Application.current.meta.get("packageName") + '/files');
+                        }
+                        if (!FileSystem.exists(Main.getDataPath() + "assets")) {
+                                Application.current.window.alert("Try copying assets/assets from apk to" + Application.current.meta.get("packageName") + " In your internal storage" + "\n" + "Press Ok To Close The App", "Instructions");
+                                System.exit(0);//Will close the game
+                        }
+                        if (!FileSystem.exists(Main.getDataPath() + "mods")) {
+                                Application.current.window.alert("Try copying assets/mods from apk to " + Application.current.meta.get("packageName") + " In your internal storage" + "\n" + "Press Ok To Close The App", "Instructions");
+                                System.exit(0);//Will close the game
+                        }
                 }
-                #end
+                #end                
+             
 
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
