018c104a07c3ad15298b5d3636284f7884c7077b
Randomxdp <95031664+Randomxdp@users.noreply.github.com>
2022-02-07 21:53:42 -0300

    E

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2205,7 +2205,7 @@ judgementCounter.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, FlxTextAli
 			scoreTxt.text = 'Score: ' + songScore + ' // Health: 50 % // Rating: ' + ratingName;
                         judgementCounter.text = 'Sicks: 0 \nGoods: 0\nBads: 0\nShits: 0\nMisses: 0\ne';
 		} else {
-			scoreTxt.text = 'Score: ' + songScore + ' // Health: ' + Math.round(health * 50) + '% // Rating: ' + ratingName + ' (' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%)' + ' - ' + ratingFC;//peeps wanted no integer rating
+			scoreTxt.text = 'Score: ' + songScore + ' // Health: ' + Math.round(health * 50) + '% // Rating: ' + ratingName + ' (' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%)' + ' // ' + ratingFC;//peeps wanted no integer rating
                         judgementCounter.text = 'Sicks: ${sicks}\nGoods: ${goods}\nBads: ${bads}\nShits: ${shits}\nMisses: ${songMisses}\ne';
 		}
 
