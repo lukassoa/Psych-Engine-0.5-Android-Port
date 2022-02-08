9bab0aa732b248ee7d09ec8a5f2c7710a1086ffd
Randomxddd <amongus@sus.com>
2022-02-08 20:40:59 -0300

    testing & fixing

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1017,18 +1017,19 @@ class PlayState extends MusicBeatState
 		scoreTxt.visible = !ClientPrefs.hideHud;
 		add(scoreTxt);
 
-		if(!ClientPrefs.noAntimash);     
+		if(!ClientPrefs.noAntimash) {
                 versionTxt = new FlxText(0, FlxG.height - 24, 0, SONG.song + " - " + CoolUtil.difficultyString() , 16);
 			versionTxt.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 			versionTxt.scrollFactor.set();
 			add(versionTxt);
-		else
+                }
+		
 		if(ClientPrefs.noAntimash);
                 versionTxt = new FlxText(0, FlxG.height - 24, 0, SONG.song + " - " + CoolUtil.difficultyString() + "| no Antimash!" , 16);
                         versionTxt.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
                         versionTxt.scrollFactor.set();
                         add(versionTxt);
-                end
+                }
                 judgementCounter = new FlxText(20, 0, 0, "", 20);
                 judgementCounter.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, FlxTextAlign.LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
                 judgementCounter.borderSize = 2;
