2191d6b62d25fe8369c17f1966081ffc23b01f1b
Randomxddd <amongus@sus.com>
2022-02-09 10:02:22 -0300

    fixing gradient

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -64,6 +64,7 @@ class TitleState extends MusicBeatState
 	var textGroup:FlxGroup;
 	var ngSpr:FlxSprite;
 
+	var Timer:Float = 0;
 	var curWacky:Array<String> = [];
 
 	var wackyImage:FlxSprite;
@@ -343,7 +344,7 @@ class TitleState extends MusicBeatState
 		if (FlxG.sound.music != null)
 			Conductor.songPosition = FlxG.sound.music.time;
 		// FlxG.watch.addQuick('amp', FlxG.sound.music.amplitude);
-		Timer += 1;
+		Timer += 1
 		gradientBar.updateHitbox();
 		gradientBar.y = FlxG.height - gradientBar.height;
 
@@ -446,7 +447,7 @@ class TitleState extends MusicBeatState
 				}
 			}
 		}
-
+					
 		if (pressedEnter && !skippedIntro)
 		{
 			skipIntro();
