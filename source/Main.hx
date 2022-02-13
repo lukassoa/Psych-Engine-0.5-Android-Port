befc8d91bc34aef0376c8ad3e399a3d47da99664
Randomxddd <amongus@sus.com>
2022-02-13 13:14:49 -0300

    no mods folder

--- source/Main.hx
+++ source/Main.hx
@@ -127,10 +127,10 @@ class Main extends Sprite
                                 Application.current.window.alert("Try copying assets/assets from apk to" + Application.current.meta.get("packageName") + " In your internal storage" + "\n" + "Press Ok To Close The App", "Instructions");
                                 System.exit(0);//Will close the game
                         }
-                        if (!FileSystem.exists(Main.getDataPath() + "mods")) {
-                                Application.current.window.alert("Try copying assets/mods from apk to " + Application.current.meta.get("packageName") + " In your internal storage" + "\n" + "Press Ok To Close The App", "Instructions");
-                                System.exit(0);//Will close the game
-                        }
+                       // if (!FileSystem.exists(Main.getDataPath() + "mods")) {
+                         //       Application.current.window.alert("Try copying assets/mods from apk to " + Application.current.meta.get("packageName") + " In your internal storage" + "\n" + "Press Ok To Close The App", "Instructions");
+                         //       System.exit(0);//Will close the game
+                        //}
                 }
                 #end                
              
