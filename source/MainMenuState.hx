74608ab40a73901faea5325e2927a47cafd36e0a
randomXD <you@example.com>
2022-03-21 13:20:17 -0300

    uh dont use this rn

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -114,7 +114,8 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.screenCenter(X);
+			menuItem.x += 100;
+//			menuItem.screenCenter(X);
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
