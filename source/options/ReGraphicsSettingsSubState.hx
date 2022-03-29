4d845b8f9cff3bca7e16a9c08a0ca528e072058f
Randomxd <you@example.com>
2022-03-29 19:40:58 -0400

    fixes

---
+++ source/options/ReGraphicsSettingsSubState.hx
@@ -0,0 +1,53 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class ReGraphicsSettingsSubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Graphics';
+		rpcTitle = 'Graphics Settings Menu'; //for Discord Rich Presence
+
+                var option:Option = new Option('Hide Gf and BG', //Name
+                        'If checked, disables gf and the background i guess??.',
+                        'hideGf', 
+                        'bool', 
+                        false); 
+                addOption(option);
+
+		var option:Option = new Option('Max Optimization', 
+                        'If checked, removes most stuff except the ui.', 
+                        'maxOpt', 
+                        'bool', 
+                        false); 
+                addOption(option);
+
+		super();
+	}
+}
