3bbf3e2ffa7115bab4bf11238e58c7f0d5fd8df7
Randomxd <you@example.com>
2022-03-28 21:02:31 -0400

    e

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2456,7 +2456,7 @@ class PlayState extends MusicBeatState
 
 		// FlxG.watch.addQuick('VOL', vocals.amplitudeLeft);
 		// FlxG.watch.addQuick('VOLRight', vocals.amplitudeRight);
-		shownHealth =  FlxMath.lerp(shownHealth, health, CoolUtil.boundTo(elapsed * 2, 0, 1));
+		shownHealth =  FlxMath.lerp(shownHealth, health, CoolUtil.boundTo(elapsed * 9, 0, 1));
 
 		var mult:Float = FlxMath.lerp(1, iconP1.scale.x, CoolUtil.boundTo(1 - (elapsed * 9), 0, 1));
 		iconP1.scale.set(mult, mult);
